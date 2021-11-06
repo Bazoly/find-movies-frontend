116650a87a9529fddaac90e670ea7219d67798b3
Zoltán Baranyay <zbaranyay@gmail.com>
2021-11-06 20:44:24 +0100

    feat: implement the GraphQL connection in the App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,40 @@
 import './App.css';
+import {AppBar} from "@mui/material";
+import Search from "./components/Search/Search";
+import {ApolloClient, InMemoryCache, ApolloProvider, HttpLink, from} from "@apollo/client";
+import {onError} from "@apollo/client/link/error";
+
+const errorLink = onError(({graphqlErrors, networkError}) => {
+    if (graphqlErrors) {
+        graphqlErrors.map(({message, location, path}) => {
+            console.log(`Graphql error ${message} ${location} ${path}`)
+        })
+    }
+});
+
+const link = from([
+    errorLink,
+    new HttpLink({uri: "https://tmdb.sandbox.zoosh.ie/dev/graphql"})
+]);
+
+const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    link: link
+});
+
 
 function App() {
-  return (
-    <div className="App">
-      Hello World!
-    </div>
-  );
+    return (
+        <main>
+            <AppBar position={"static"}>Find Movies</AppBar>
+            <ApolloProvider client={client} >
+                <Search />
+            </ApolloProvider>
+
+
+        </main>
+
+    );
 }
 
 export default App;
