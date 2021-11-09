aaed1ee4f57ba09c3d7a0b5ffed6def274a5b0e8
Zoltán Baranyay <zbaranyay@gmail.com>
2021-11-09 14:20:32 +0100

    feat: implement the getMovieFirstParagraph() method in the fetchMovieDetails.js file

--- src/api/rest/fetchMovieDetails.js
+++ src/api/rest/fetchMovieDetails.js
@@ -1,8 +1,9 @@
 const WIKIPEDIA_API = `https://en.wikipedia.org/w/api.php?`;
 const SEARCH_MOVIE_QUERY = `&origin=*&format=json&action=query&list=search&srsearch=`
+const GET_MOVIE_BY_PAGE_ID = `&origin=*&format=json&action=query&prop=categories|extracts&exintro&explaintext&redirects=1&pageids=`
 const FIRST_RESULT_INDEX = 0;
 
-export default async function searchMovie(movieTitle) {
+async function searchMovie(movieTitle) {
     try {
         const response = await fetch(WIKIPEDIA_API + SEARCH_MOVIE_QUERY + movieTitle);
         return getMoviePageId(await response.json());
@@ -14,4 +15,18 @@ export default async function searchMovie(movieTitle) {
 function getMoviePageId(movieList) {
     return movieList.query.search[FIRST_RESULT_INDEX].pageid;
 }
+
+export default async function getMovieFirstParagraph(movieTitle) {
+    const pageId = await searchMovie(movieTitle);
+    try {
+        const response = await fetch(WIKIPEDIA_API + GET_MOVIE_BY_PAGE_ID + pageId);
+        const movie = await response.json();
+        return movie.query.pages[pageId];
+    } catch (e) {
+        console.log(e);
+    }
+
+}
+
+
 
