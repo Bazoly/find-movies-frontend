6e0081afa14256b5a5dca3dcc942343255c73bf3
Zoltán Baranyay <zbaranyay@gmail.com>
2021-11-09 13:14:41 +0100

    refactor: change constants variable names to uppercase letter

--- src/api/rest/fetchMovieDetails.js
+++ src/api/rest/fetchMovieDetails.js
@@ -1,10 +1,10 @@
-const wikipediaApi = `https://en.wikipedia.org/w/api.php?`;
-const searchMovieQuery = `&origin=*&format=json&action=query&list=search&srsearch=`
+const WIKIPEDIA_API = `https://en.wikipedia.org/w/api.php?`;
+const SEARCH_MOVIE_QUERY = `&origin=*&format=json&action=query&list=search&srsearch=`
 const FIRST_RESULT_INDEX = 0;
 
 export default async function searchMovie(movieTitle) {
     try {
-        const response = await fetch(wikipediaApi + searchMovieQuery + movieTitle);
+        const response = await fetch(WIKIPEDIA_API + SEARCH_MOVIE_QUERY + movieTitle);
         return getMoviePageId(await response.json());
     } catch (e) {
         console.log(e);
