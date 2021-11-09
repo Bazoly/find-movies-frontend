73d306e9f569554b0526b49aae1c84aace7e28ae
Zoltán Baranyay <zbaranyay@gmail.com>
2021-11-09 14:49:17 +0100

    feat: the first Wikipedia paragraph appear in the MovieDetailsContainer component

--- src/components/MovieDetailsContainer/MovieDetailsContainer.jsx
+++ src/components/MovieDetailsContainer/MovieDetailsContainer.jsx
@@ -1,9 +1,30 @@
 import {useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
+import getMovieFirstParagraph from "../../api/rest/fetchMovieDetails";
+import {CircularProgress} from "@mui/material";
 
-export default function MovieDetailsContainer () {
+export default function MovieDetailsContainer() {
     const {movieTitle} = useParams();
+    const [movieDetails, setMovieDetails] = useState({});
+    const [loading, setLoading] = useState(true);
 
+    useEffect(() => {
+        async function fetchData() {
+            const data = await getMovieFirstParagraph(movieTitle);
+            setLoading(false)
+            setMovieDetails(data);
+        }
+
+        fetchData()
+    }, [movieTitle]);
+
+    if (loading) return <CircularProgress/>
     return (
-        <h2>{movieTitle}</h2>
+        <div>
+            <h2>{movieDetails.title}</h2>
+            <p>{movieDetails.extract}</p>
+        </div>
+
+
     );
 }
