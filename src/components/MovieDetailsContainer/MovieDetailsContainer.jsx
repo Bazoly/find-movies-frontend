10d6fb668894105c261c0dbaa1930362ecbce940
Zoltán Baranyay <zbaranyay@gmail.com>
2021-11-10 12:50:15 +0100

    feat: add the Wikipedia link and the IMDB link to the MovieDetailsContainer component

--- src/components/MovieDetailsContainer/MovieDetailsContainer.jsx
+++ src/components/MovieDetailsContainer/MovieDetailsContainer.jsx
@@ -23,6 +23,12 @@ export default function MovieDetailsContainer() {
         <div>
             <h2>{movieDetails.title}</h2>
             <p>{movieDetails.extract}</p>
+            <a title={"Wikipedia: " + movieDetails.title} href={movieDetails.canonicalurl} target={"_blank"}>
+                <img height="50" alt="Wikipedia&#039;s W" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Wikipedia%27s_W.svg/128px-Wikipedia%27s_W.svg.png"/>
+            </a>
+            <a title={"IMDB: " + movieDetails.title} href={movieDetails.imdbLink} target={"_blank"}>
+                <img height="50" alt="IMDB Logo 2016" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/512px-IMDB_Logo_2016.svg.png"/>
+            </a>
         </div>
 
 
