8ce80bbfd70150315ae573ed9f707732bb34b236
Zoltán Baranyay <zbaranyay@gmail.com>
2021-11-11 13:35:26 +0100

    feat: replace standard html tags to Material UI's Card components in the MovieDetailsContainer.jsx file

--- src/components/MovieDetailsContainer/MovieDetailsContainer.jsx
+++ src/components/MovieDetailsContainer/MovieDetailsContainer.jsx
@@ -1,7 +1,7 @@
 import {useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
 import getMovieFirstParagraph from "../../api/rest/fetchMovieDetails";
-import {CircularProgress} from "@mui/material";
+import {Card, CardActions, CardContent, CardHeader, CircularProgress, IconButton} from "@mui/material";
 
 export default function MovieDetailsContainer() {
     const {movieSearchParams} = useParams();
@@ -21,16 +21,22 @@ export default function MovieDetailsContainer() {
     if (loading) return <CircularProgress/>
     if (movieDetails === undefined) return <h2>Movie not found!</h2>
     return (
-        <div>
-            <h2>{movieDetails.title}</h2>
-            <p>{movieDetails.extract}</p>
-            <a title={"Wikipedia: " + movieDetails.title} href={movieDetails.canonicalurl} target={"_blank"}>
-                <img height="50" alt="Wikipedia&#039;s W" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Wikipedia%27s_W.svg/128px-Wikipedia%27s_W.svg.png"/>
-            </a>
-            <a title={"IMDB: " + movieDetails.title} href={movieDetails.imdbLink} target={"_blank"}>
-                <img height="50" alt="IMDB Logo 2016" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/512px-IMDB_Logo_2016.svg.png"/>
-            </a>
-        </div>
+        <Card elevation={3}>
+            <CardHeader title={movieDetails.title}/>
+            <CardContent>{movieDetails.extract}</CardContent>
+            <CardActions>
+                <IconButton title={"Wikipedia: " + movieDetails.title} href={movieDetails.canonicalurl}
+                            target={"_blank"} rel={"noreferrer"}>
+                    <img height="25" alt="Wikipedia&#039;s W"
+                         src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Wikipedia%27s_W.svg/128px-Wikipedia%27s_W.svg.png"/>
+                </IconButton>
+                <IconButton title={"IMDB: " + movieDetails.title} href={movieDetails.imdbLink} target={"_blank"}
+                            rel={"noreferrer"}>
+                    <img height="25" alt="IMDB Logo 2016"
+                         src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/512px-IMDB_Logo_2016.svg.png"/>
+                </IconButton>
+            </CardActions>
+        </Card>
 
 
     );
