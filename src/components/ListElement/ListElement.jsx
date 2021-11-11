8f305bc8c619ae44f03f69a7abc843146b3f6689
Zoltán Baranyay <zbaranyay@gmail.com>
2021-11-11 13:34:23 +0100

    feat: replace standard html tags to Material UI's Card components

--- src/components/ListElement/ListElement.jsx
+++ src/components/ListElement/ListElement.jsx
@@ -1,3 +1,4 @@
+import {Card, CardContent, CardHeader} from "@mui/material";
 import {Link} from "react-router-dom";
 import "./ListElement.css"
 
@@ -10,15 +11,19 @@ export default function ListElement({movie}) {
     }
 
     return (
-        <div key={movie.id}>
-            <h3>
-                <Link to={`/movie/${movie.name} ${releaseDate?.getFullYear()} ${movie.cast[FIRST_CAST_MEMBER_INDEX]?.person.name}`}>
+        <Card key={movie.id} sx={{m: 1}} elevation={3}>
+            <CardHeader title={
+                <Link
+                    to={`/movie/${movie.name} ${releaseDate?.getFullYear()} ${movie.cast[FIRST_CAST_MEMBER_INDEX]?.person.name}`}>
                     {movie.name}
                 </Link>
-            </h3>
-            <p>Release date: {releaseDate?.toLocaleDateString()}</p>
-            <p>Score: {movie.score}</p>
-            <p>Genre: {movie.genres.map((genre) => genre.name + " ")}</p>
-        </div>
+            }>
+            </CardHeader>
+            <CardContent>
+                <p>Release date: {releaseDate?.toLocaleDateString()}</p>
+                <p>Score: {movie.score}</p>
+                <p>Genre: {movie.genres.map((genre, i) => movie.genres.length === i + 1 ? genre.name : genre.name + ", ")}</p>
+            </CardContent>
+        </Card>
     )
 }
