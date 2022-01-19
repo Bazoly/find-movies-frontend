7a96b4b6e832c079002b0bcbdc2b0980f648493a
Zoltán Baranyay <zbaranyay@gmail.com>
2022-01-19 09:44:57 +0100

    feat: migrate the ListContainer component from JavaScript to TypeScript

--- src/components/ListContainer/ListContainer.tsx
+++ src/components/ListContainer/ListContainer.tsx
@@ -1,20 +1,33 @@
+import React, {ReactElement} from "react";
 import ListElement from "../ListElement/ListElement";
 import {useQuery} from "@apollo/client";
 import {SEARCH_MOVIES_BY_TITLE} from "../../api/graphql/queries";
 import {useSearchParams} from "react-router-dom";
 import {CircularProgress} from "@mui/material";
 
-export default function ListContainer() {
+export interface Movie {
+    id: number;
+    name: string;
+    releaseDate: string;
+    genres: Genre[];
+    score: number
+}
+
+interface Genre {
+    name: string
+}
+
+export default function ListContainer(): ReactElement {
     const [searchParams, setSearchParams] = useSearchParams();
     const {error, loading, data} = useQuery(SEARCH_MOVIES_BY_TITLE, {
         variables: {title: searchParams.get("movie")},
     });
 
     if (loading) return <CircularProgress/>;
-    if (error) return `Error: ${error.message}`
+    if (error) return <p>`Error: ${error.message}`</p>
     return (
         <div>
-            {data.searchMovies.map((movie) => {
+            {data.searchMovies.map((movie: Movie) => {
                 return <ListElement key={movie.id} movie={movie}/>
             })}
         </div>
